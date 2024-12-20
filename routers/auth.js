93f949cf8817291e670c2d8e8a6af07e5d9ae23f
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-12-20 14:29:52 +0500

    create auth route.

--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import User from '../models/User.js';
+import bcrypt from 'bcrypt'
+import Joi from 'joi'
+
 const router = express.Router();
+
+const schema = Joi.object({
+    email: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    fullname: Joi.string().alphanum().min(3).max(30).required(),
+})
 
 router.post('/register', (req, res) => {
     const { fullname, email, password } = req.body;
-
+    
 })
 
 router.post('/login', (req, res) => {
